f4e4183f5c0cee46917119ae166371cd82278a47
Pritesh Mann <priteshmann99@gmail.com>
2025-09-26 03:12:12 +0530

    Fix TypeScript build errors

--- src/app/trial-expired/page.tsx
+++ src/app/trial-expired/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import TrialExpiredPage from '@/app/trial-expired/TrialExpiredPage';
 
 export default function TrialExpiredPageRoute() {
